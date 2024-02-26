200b7e8c9e056ef379666d0208d45c4303bf2d27
Leonardo <lgarc168@itam.mx>
2024-02-25 20:19:41 -0600

    Final

--- ProyectoEstructurasFinal/src/Convertir.java
+++ ProyectoEstructurasFinal/src/Convertir.java
@@ -1,10 +1,22 @@
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author Leonardo García Bernal
+ */
 public class Convertir {
+
+    /**
+     *
+     */
     public Convertir() {
 
     }
-
+/**
+ * 
+ * @param chr Recibe un caractér cualquiera.
+ * @return Regresa la jerarquía del orden de ejecución de operadores aritméticos
+ * por convención si el peramétro es un operador,sino es operador regresa -1;
+ */
     private static int JerarquiaOperaciones(char chr) {
         byte var;
         switch (chr) {
@@ -25,7 +37,13 @@ public class Convertir {
         }
         return var;
     }
-
+/**
+ * 
+ * @param str Recibe la cadena de caracteres que contiene la operación aritmética que se quiere
+ * convertir a posfia.
+ * @return Regresa un ArrayList de los caracteres individuales del String dado como parámetro ordenados
+ * en forma posfija.
+ */
     public static ArrayList<String> Convertir(String str) {
         ArrayList<String> arrayList = new ArrayList();
         ArregloPila<Character> pila = new ArregloPila();
